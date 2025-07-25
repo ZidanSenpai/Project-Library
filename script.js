9436a1d1cd19c931ee7acb981a84e5c56391f850
Zidan <zidan.senpaii@gmail.com>
2025-07-26 02:27:03 +0530

    add basic functionality

--- script.js
+++ script.js
@@ -8,5 +8,32 @@ function addBook(name, author){
     const newBook = new Book(name, author);
     bookArray.push(newBook);
 }
+const container = document.getElementById("container");
+function displayBooks(){
+    for (let i = 0; i < bookArray.length; i++) {
+        const element = bookArray[i];
+        container.appendChild(element);
+    }
+}
+const addbtn = document.getElementById("add-button");
+const bookForm = document.getElementById("book-form");
+
+addbtn.addEventListener("click", ()=>{
+    bookForm.style.display = "block"; // Show the form
+})
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // Prevent page reload
+
+    const name = document.getElementById("book-name").value;
+    const author = document.getElementById("book-author").value;
+    addBook(name, author)
+    // Add your book-adding logic here
+    console.log("Book Added:", name, author);
+
+    // Clear and hide the form
+    bookForm.reset();
+    bookForm.style.display = "none";
+});
 addBook("Crime and Punishment", "Fyodor Dostoevsky");
 console.log(bookArray);
+displayBooks();
